034dbd9df8f2058bcd039dde612abb6ff730a106
Tommi Lätti <styygeli@users.noreply.github.com>
2023-11-03 19:11:24 +0900

    clean-up, add stuff
    Cleaned up the code a lot
    
    added comments
    
    added new aircon in the AV room (summers were getting hot!) ;)

--- panasonic_pull.rb
+++ panasonic_pull.rb
@@ -1,53 +1,73 @@
 #!/usr/bin/ruby
 
-require 'rubygems'
+# This script fetches data from a Panasonic breakerbox, processes it, and generates Prometheus metrics for a specific use case.
+
 require 'open-uri'
 require 'csv'
 
-csv_data = CSV.parse(URI.open("http://192.168.0.59/csv/InstVal.csv"), :headers => :first_row)
+# Fetch data from a remote CSV file using its URL and treat the first row as headers.
+csv_data = CSV.parse(URI.open("http://192.168.0.59/csv/InstVal.csv"), headers: :first_row)
 
-power = {
-	"main" => csv_data[5][1].to_i(16)*10,
-	"pv1" => csv_data[5][2].to_i(16),
-	"ecocute" => csv_data[5][5].to_i(16)*10,
-	"living_lights" => csv_data[5][17].to_i(16),
-	"utility_lights" => csv_data[5][18].to_i(16),
-	"mbr_wic_lights" => csv_data[5][19].to_i(16),
-	"av_lights" => csv_data[5][20].to_i(16),
-	"office_lights" => csv_data[5][21].to_i(16),
-	"jin_lights" => csv_data[5][22].to_i(16),
-	"bath_toilet_lights" => csv_data[5][23].to_i(16),
-	"hall_toilet_outlet" => csv_data[5][24].to_i(16),
-	"living_pantry_outlet" => csv_data[5][25].to_i(16),
-	"jin_outlet" => csv_data[5][26].to_i(16),
-	"mbr_outlet" => csv_data[5][27].to_i(16),
-	"freezer" => csv_data[5][28].to_i(16),
-	"office_outlet" => csv_data[5][29].to_i(16),
-	"washing_machine" => csv_data[5][30].to_i(16),
-	"av_outlet_left" => csv_data[5][31].to_i(16),
-	"av_outlet_right" => csv_data[5][32].to_i(16),
-	"utility_outlet_left" => csv_data[5][33].to_i(16),
-	"utility_outlet_right" => csv_data[5][34].to_i(16),
-	"bath_outlet" => csv_data[5][35].to_i(16),
-	"toilet_outlet" => csv_data[5][36].to_i(16),
-	"microwave" => csv_data[5][37].to_i(16),
-	"kitchen_counter" => csv_data[5][38].to_i(16),
-	"septic" => csv_data[5][39].to_i(16),
-	"diswasher" => csv_data[5][40].to_i(16),
-	"stiebel" => csv_data[5][41].to_i(16),
-	"aircon" => csv_data[5][42].to_i(16),
-	"stove" => csv_data[5][43].to_i(16),
-	"ev_charger" => csv_data[5][48].to_i(16),
-	"oven" => csv_data[5][49].to_i(16)
-	}
+# Define a mapping between metric names and their corresponding column indices in the CSV data.
+power_mappings = {
+  "main" => 1,
+  "pv1" => 2,
+  "ecocute" => 5,
+  "living_lights" => 17,
+  "utility_lights" => 18,
+  "mbr_wic_lights" => 19,
+  "av_lights" => 20,
+  "office_lights" => 21,
+  "jin_lights" => 22,
+  "bath_toilet_lights" => 23,
+  "hall_toilet_outlet" => 24,
+  "living_pantry_outlet" => 25,
+  "jin_outlet" => 26,
+  "mbr_outlet" => 27,
+  "freezer" => 28,
+  "office_outlet" => 29,
+  "washing_machine" => 30,
+  "av_outlet_left" => 31,
+  "av_outlet_right" => 32,
+  "utility_outlet_left" => 33,
+  "utility_outlet_right" => 34,
+  "bath_outlet" => 35,
+  "toilet_outlet" => 36,
+  "microwave" => 37,
+  "kitchen_counter" => 38,
+  "septic" => 39,
+  "diswasher" => 40,
+  "stiebel" => 41,
+  "aircon" => 42,
+  "stove" => 43,
+  "av_ac" => 44,
+  "ev_charger" => 48,
+  "oven" => 49
+}
 
+# Begin writing the generated metrics to a Prometheus data file.
 begin
-	file = File.open("/var/lib/prometheus/node-exporter/panasonic.prom", "w")
-	power.each do |key, value|
-		friendly_name = key.to_s.split('_').map(&:capitalize).join(' ')
-		file.write("power_w" "{domain=\"panasonic\"" ",entity=\"" "#{key}\"" ",friendly_name=\"" "#{friendly_name}\"}" " #{value}\n")
-	end
+  # Open the file for writing. The file will be created or overwritten.
+  file = File.open("/var/lib/prometheus/node-exporter/panasonic.prom", "w")
+
+  # Iterate through the power_mappings and process the data for each metric.
+  power_mappings.each do |key, column|
+
+    # Read the value from the CSV data, converting it from hexadecimal to an integer.
+    value = csv_data[5][column].to_i(16)
+
+    # Multiply the value by 10 for "main" and "ecocute" metrics.
+    value *= 10 if key == "main" || key == "ecocute"
+
+    # Generate the friendly name by capitalizing words separated by underscores.
+    friendly_name = key.to_s.split('_').map(&:capitalize).join(' ')
+
+    # Write the Prometheus metric to the file, using string interpolation.
+    file.write("power_w{domain=\"panasonic\",entity=\"#{key}\",friendly_name=\"#{friendly_name}\"} #{value}\n")
+  end
 rescue IOError => e
+  # Handle any IO-related errors and exceptions, such as file access problems.
 ensure
-	file.close unless file.nil?
+  # Ensure that the file is closed after writing (if it was opened).
+  file&.close
 end
